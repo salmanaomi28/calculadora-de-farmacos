269ade9524af30566d1fbaf811776f388144c0ca
salmanaomi28 <100600590+salmanaomi28@users.noreply.github.com>
2022-03-09 02:21:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def get_datosD(peso,dosisTerapeutica):
 
 
 @app.route("/suspension/<string:peso>/<string:presentacion>/<string:dosisTerapeutica>/<string:diluyente>")
+    def get_datosM(peso,presentacion,dosisTerapeutica, diluyente):
     dosisTerapeutica = float(dosisTerapeutica)
     diluyente = float(diluyente)
     peso = float(peso)
