b6ad1dbda2ebab13cb522fc4e14b6c8e0cf9230e
salmanaomi28 <100600590+salmanaomi28@users.noreply.github.com>
2022-03-09 03:11:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
 from distutils.log import debug
+from flask import Flask
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 
 @app.route("/dosis/<string:peso>/<string:dosisT>")
@@ -10,30 +10,33 @@ def get_datosD(peso,dosisT):
     peso = float(peso)
     dosisT = float(dosisT)
     dosis = dosisT * peso
-    return jsonify({ "dosis" : dosis})
+    return jsonify({"dosis" : dosis})
 
 
-@app.route("/susp/<string:peso>/<string:mg>/<string:dosisT>/<string:ml>")
-    def get_datosM(peso,mg,dosisT, ml):
+@app.route("/suspension/<string:peso/<string:mg>/<string:dosisT>/<string:ml>")
+def get_DatosM(peso, mg, dosisT, ml):
+    peso = float(peso)
+    mg = float(mg)
     dosisT = float(dosisT)
     ml = float(ml)
-    peso = float(peso)
-    dosis = float(dosis)
-    mg = float(mg)
+    dosis = float(0)
     dosis = dosisT * peso
-    susp = dosisT * ml / mg
-    return jsonify({"susp": susp})
+    suspension = dosis * (ml/mg)
+    return jsonify({"suspension" : suspension})
 
-@app.route("/dosisPordia/<string:dosisT>/<string:peso>/<string:horasM>")
-def get_datosM2(peso,dosisT, horasM):
-    cc = float(0)
+
+
+@app.route("/dosisDia/<string:dosisT>/<string:peso>/<string:peso>/<string:horasM>")
+def get_dosisDia(dosisT,peso,horasM):
     dosisT = float(dosisT)
     peso = float(peso)
     horasM = float(horasM)
-    dosis = dosisT * peso
+    cc = float(0)
     cc = 24 / horasM
-    dosisPorDia = dosis / cc
-    return jsonify({"dosisPorDia":dosisPorDia})
+    dosisDia = dosis / cc
+    return jsonify({"dosisDia" : dosisDia})
 
-if __name__ == '__main__':
+
+
+if _name_ == '__main__':
     app.run(debug=True, port=4000)
